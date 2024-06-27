f621889104e03386c3aa72a947667c022636a26d
OSIELJ <osieljuniormartins@gmail.com>
2024-06-27 10:51:57 -0300

    feat: Implementa navegação com NavController e BottomNavigationView usando View Binding
    - Configurado o activity_main.xml com NavHostFragment e BottomNavigationView corretamente.
    - Implementada a MainActivity para obter o NavHostFragment usando supportFragmentManager e inicializar o NavController.
    - Utilizado View Binding para acessar BottomNavigationView de forma segura.
    - Configurado BottomNavigationView com NavController para navegação.
    
    Essas alterações implementam a navegação de forma correta e eficiente, utilizando as melhores práticas com View Binding.

---
+++ app/src/main/java/com/example/eatfood/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.eatfood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.eatfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.frag_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = binding.bottomNavigation
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
