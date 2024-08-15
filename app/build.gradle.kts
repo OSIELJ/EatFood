36d52904b0bafb141cbf1b94bb1786586a45ad02
OSIELJ <osieljuniormartins@gmail.com>
2024-08-15 10:45:08 -0300

    Alterando dependencias depreciadas.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
     val nav_version = "2.7.7"
+    val lifecycle_version = "2.4.0-rc01"
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,22 +51,24 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
+    // Intuit SDP e SSP
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
 
-    //intuit
-    implementation ("com.intuit.sdp:sdp-android:1.0.6")
-    implementation ("com.intuit.ssp:ssp-android:1.0.6")
+    // GIF Drawable
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
 
-    //gif
-    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
-
-    //retrofit
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
 
-    //glide
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+
+    // AndroidX Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 }
