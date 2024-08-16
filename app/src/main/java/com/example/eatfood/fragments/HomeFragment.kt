4198d651f50cce5b744b31b5e4beb617f2b75acf
OSIELJ <osieljuniormartins@gmail.com>
2024-08-16 10:29:32 -0300

    Logicas para funcionalidades.

---
+++ app/src/main/java/com/example/eatfood/fragments/HomeFragment.kt
@@ -0,0 +1,65 @@
+package com.example.eatfood.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.eatfood.activites.MealActivity
+import com.example.eatfood.data.Meal
+import com.example.eatfood.databinding.FragmentHomeBinding
+import com.example.eatfood.viewModel.HomeViewModel
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeMvvm:HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeMvvm = ViewModelProvider(this)[HomeViewModel::class.java]
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeMvvm.getRandomMeal()
+        observerRandomMeal()
+        onRandomMealClick()
+
+
+    }
+
+    private fun onRandomMealClick() {
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    private fun observerRandomMeal() {
+        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner
+        ) { value ->
+            Glide.with(this@HomeFragment)
+                .load(value.strMealThumb)
+                .into(binding.imgRandomMeal)
+        }
+
+    }
+
+
+}
