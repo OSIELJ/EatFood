07a821ca4d9bf075b85966c70d702e6f13d74969
OSIELJ <osieljuniormartins@gmail.com>
2024-08-20 07:41:48 -0300

    Criando lógica para mostrar detalhes da refeição na tela.

--- app/src/main/java/com/example/eatfood/activites/MealActivity.kt
+++ app/src/main/java/com/example/eatfood/activites/MealActivity.kt
@@ -1,20 +1,30 @@
 package com.example.eatfood.activites
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.eatfood.R
+import com.example.eatfood.data.Meal
 import com.example.eatfood.databinding.ActivityMealBinding
 import com.example.eatfood.fragments.HomeFragment
+import com.example.eatfood.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
+    private lateinit var youtubeLink:String
     private lateinit var binding:ActivityMealBinding
+    private lateinit var mealMvvm:MealViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
@@ -26,9 +36,33 @@ class MealActivity : AppCompatActivity() {
             insets
         }
 
+        mealMvvm = ViewModelProvider(this)[MealViewModel::class.java]
+
         getMealInformationFromIntent()
 
         setInformationInViews()
+
+        mealMvvm.getMealDetails(mealId)
+        observerMealDetailsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+    }
+
+    private fun observerMealDetailsLiveData() {
+        mealMvvm.observeMealDetailsLiveData().observe(this) {meal ->
+            binding.tvCategoryInfo.text = "Category : ${meal.strCategory}"
+            binding.tvAreaInfo.text = "Area : ${meal.strArea}"
+            binding.tvInstructions.text = meal.strInstructions
+
+            youtubeLink = meal.strYoutube.toString()
+        }
     }
 
     private fun setInformationInViews() {
@@ -46,4 +80,6 @@ class MealActivity : AppCompatActivity() {
         mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
     }
+
+
 }
