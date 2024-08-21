4015f2d138adb9f742714119a27e71f0659d8dd0
OSIELJ <osieljuniormartins@gmail.com>
2024-08-21 08:17:55 -0300

    Preparando o recycler view e ao clicar em um card passar informações para outra activity e mostrar as informações da refeição daquele card.

--- app/src/main/java/com/example/eatfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/eatfood/fragments/HomeFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.eatfood.activites.MealActivity
+import com.example.eatfood.adapters.MostPopularAdapter
+import com.example.eatfood.data.MealsByCategory
 import com.example.eatfood.data.Meal
 import com.example.eatfood.databinding.FragmentHomeBinding
 import com.example.eatfood.viewModel.HomeViewModel
@@ -19,6 +21,7 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var homeMvvm:HomeViewModel
     private lateinit var randomMeal: Meal
+    private lateinit var popularItemsAdapter: MostPopularAdapter
 
     companion object{
         const val MEAL_ID = "com.example.eatfood.fragments.idMeal"
@@ -29,6 +32,8 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         homeMvvm = ViewModelProvider(this)[HomeViewModel::class.java]
+
+        popularItemsAdapter = MostPopularAdapter()
 
     }
 
@@ -43,10 +48,43 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        preparePopularItemsRecyclerView()
+
         homeMvvm.getRandomMeal()
         observerRandomMeal()
         onRandomMealClick()
 
+
+        homeMvvm.getPopularItems()
+        observePopularItemsLiveData()
+        onPopularItemClick()
+
+
+    }
+
+    private fun onPopularItemClick() {
+        popularItemsAdapter.onItemClick = { meal ->
+            val intent = Intent(activity, MealActivity::class.java)
+            intent.putExtra(MEAL_ID, meal.idMeal)
+            intent.putExtra(MEAL_NAME, meal.strMeal)
+            intent.putExtra(MEAL_THUMB, meal.strMealThumb)
+            startActivity(intent)
+        }
+    }
+
+    private fun preparePopularItemsRecyclerView() {
+        binding.recViewMealsPopular.apply {
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            adapter = popularItemsAdapter
+        }
+    }
+
+    private fun observePopularItemsLiveData() {
+        homeMvvm.observePopularItemsLiveData().observe(viewLifecycleOwner
+        ) { mealList ->
+
+            popularItemsAdapter.setMeals(mealsList = mealList as ArrayList<MealsByCategory>)
+        }
 
     }
 
--- app/src/main/java/com/example/eatfood/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/eatfood/viewModel/HomeViewModel.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.eatfood.data.MealsByCategoryList
+import com.example.eatfood.data.MealsByCategory
 import com.example.eatfood.data.Meal
 import com.example.eatfood.data.MealList
 import com.example.eatfood.retrofit.RetrofitInstance
@@ -13,6 +15,7 @@ import retrofit2.Response
 
 class HomeViewModel():ViewModel() {
     private var randomMealLiveData = MutableLiveData<Meal>()
+    private var popularItemsLiveData = MutableLiveData<List<MealsByCategory>>()
     fun getRandomMeal(){
 
         RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
@@ -28,7 +31,27 @@ class HomeViewModel():ViewModel() {
         })
     }
 
+    fun getPopularItems(){
+        RetrofitInstance.api.getPopularItems("Seafood").enqueue(object : Callback<MealsByCategoryList>{
+            override fun onResponse(call: Call<MealsByCategoryList>, response: Response<MealsByCategoryList>) {
+                response.body()?.let {
+                    popularItemsLiveData.value = response.body()!!.meals
+                }
+            }
+
+            override fun onFailure(call: Call<MealsByCategoryList>, t: Throwable) {
+                Log.d("HomeFragment", t.message.toString())
+            }
+
+        })
+    }
+
     fun observeRandomMealLiveData(): LiveData<Meal> {
         return randomMealLiveData
     }
+
+    fun observePopularItemsLiveData():LiveData<List<MealsByCategory>>{
+        return popularItemsLiveData
+    }
+
 }
