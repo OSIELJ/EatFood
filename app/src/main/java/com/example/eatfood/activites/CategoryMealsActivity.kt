8d4e04aa48fdee0831e19dd1361be172da067a72
OSIELJ <osieljuniormartins@gmail.com>
2024-08-22 10:34:38 -0300

    Todas as criações necessarias para ter um fluxo de refeições por categorias.

---
+++ app/src/main/java/com/example/eatfood/activites/CategoryMealsActivity.kt
@@ -0,0 +1,52 @@
+package com.example.eatfood.activites
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.eatfood.R
+import com.example.eatfood.adapters.CategoryMealsAdapter
+import com.example.eatfood.databinding.ActivityCategoryMealsBinding
+import com.example.eatfood.fragments.HomeFragment
+import com.example.eatfood.viewModel.CategoryMealsViewModel
+
+class CategoryMealsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCategoryMealsBinding
+    lateinit var categoryMealsViewModel: CategoryMealsViewModel
+    lateinit var categoryMealsAdapter: CategoryMealsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCategoryMealsBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        prepareRecyclerView()
+
+        categoryMealsViewModel = ViewModelProvider(this)[CategoryMealsViewModel::class.java]
+
+        categoryMealsViewModel.getMealsByCategory(intent.getStringExtra(HomeFragment.CATEGORY_NAME)!!)
+
+        categoryMealsViewModel.observeMealsLiveData().observe(this) { mealsList ->
+            binding.tvCategoryCount.text = mealsList.size.toString()
+            categoryMealsAdapter.setMealsList(mealsList)
+        }
+    }
+
+    private fun prepareRecyclerView() {
+        categoryMealsAdapter = CategoryMealsAdapter()
+        binding.rvMeals.apply {
+            layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+            adapter = categoryMealsAdapter
+        }
+    }
+}
