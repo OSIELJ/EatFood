3b5c3b516a4e4bfcab03356401842cc45bd80270
OSIELJ <osieljuniormartins@gmail.com>
2024-08-27 08:47:01 -0300

    Configurações para coroutines no viewmodel.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     // AndroidX Lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
 
     // Room
     implementation("androidx.room:room-runtime:$room_version")
