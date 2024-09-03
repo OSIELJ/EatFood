8fcdb8c6d90b8595e8bd8cbc79c707d3f4b94013
OSIELJ <osieljuniormartins@gmail.com>
2024-09-03 07:05:55 -0300

    Requisição por id para pegar refeição e adicionar em um LiveData para ser observado.

--- app/src/main/java/com/example/eatfood/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/eatfood/viewModel/HomeViewModel.kt
@@ -25,6 +25,8 @@ class HomeViewModel(
     private var popularItemsLiveData = MutableLiveData<List<MealsByCategory>>()
     private var categoriesLiveData = MutableLiveData<List<Category>>()
     private var favoriteMealsLiveData = mealDatabase.mealDao().getAllMeals()
+    private var bottomSheetMealLiveData = MutableLiveData<Meal>()
+
     fun getRandomMeal(){
 
         RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
@@ -72,6 +74,23 @@ class HomeViewModel(
 
     }
 
+    fun getMealById(id: String){
+        RetrofitInstance.api.getMealDetails(id).enqueue(object : Callback<MealList>{
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                val meal = response.body()?.meals?.first()
+                meal?.let { meal ->
+                    bottomSheetMealLiveData.postValue(meal)
+                }
+            }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.e("HomeViewModel", t.message.toString())
+            }
+
+        })
+
+    }
+
     fun deleteMeal(meal: Meal){
         viewModelScope.launch {
             mealDatabase.mealDao().delete(meal)
@@ -99,5 +118,7 @@ class HomeViewModel(
     fun observeFavoritesMealsLiveData(): LiveData<List<Meal>>{
         return favoriteMealsLiveData
     }
+
+    fun observeBottomSheetMeal(): LiveData<Meal> = bottomSheetMealLiveData
 
 }
