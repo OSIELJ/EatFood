ab1b3317fb5647475fa5ea5f415da22c37171d6e
OSIELJ <osieljuniormartins@gmail.com>
2024-10-03 09:18:56 -0300

    feat: Adicionando lõgica de click na tela de categorias(CategoriesFragment)
    - onCategoryClick() chama o adapter e o onItemClick e faz a navegação pra CategoryMealsActivity mandando putExtra.

--- app/src/main/java/com/example/eatfood/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/eatfood/fragments/CategoriesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.eatfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.eatfood.R
+import com.example.eatfood.activites.CategoryMealsActivity
 import com.example.eatfood.activites.MainActivity
 import com.example.eatfood.adapters.CategoriesAdapter
 import com.example.eatfood.databinding.FragmentCategoriesBinding
@@ -39,6 +41,8 @@ private lateinit var viewModel: HomeViewModel
 
         prepareCategoriesRecyclerView()
         observeCategoriesLiveData()
+
+        onCategoryClick()
     }
 
     private fun observeCategoriesLiveData() {
@@ -53,6 +57,15 @@ private lateinit var viewModel: HomeViewModel
         binding.rvCategories.apply {
             layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
             adapter = categoriesAdapter
+        }
+    }
+
+
+    private fun onCategoryClick() {
+        categoriesAdapter.onItemClick = { category ->
+            val intent = Intent(activity, CategoryMealsActivity::class.java)
+            intent.putExtra(HomeFragment.CATEGORY_NAME, category.strCategory)
+            startActivity(intent)
         }
     }
 
